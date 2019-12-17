648201392ad321a4423a03088919bf05ab81de19
V-gy <vgy@hotmail.fr>
2019-12-17 13:09:43 +0100

    luke film

---
+++ javascript-async1/readfiles-async.js
@@ -0,0 +1,13 @@
+// readfiles-async.js
+const fs = require('fs');
+
+fs.readdir('.', (err, files) => {
+    console.log(files);
+    console.log('Read dir - Done');
+    fs.readFile(files[0], 'utf8', (fileErr, content) => {
+      console.log('Read file - Done');
+      console.log('---', files[0], '---');
+      console.log(content);
+    });
+    console.log('Read file - Started');
+  });
---
+++ javascript-async1/request-url.js
@@ -0,0 +1,18 @@
+var request = require('request');
+request(' https://swapi.co/api/people/1/', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    const luke = JSON.parse(body)
+    console.log(luke)
+    console.log(luke.films)
+
+    request(' https://swapi.co/api/people/1/', function (error, response, body) {
+        console.log('error2:', error); // Print the error if one occurred
+        console.log('statusCode2:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body2:', body); // Print the HTML for the Google homepage.
+        const luke = JSON.parse(body)
+        console.log("Luke First Film: " + luke.films[0])
+    })
+}
+);
