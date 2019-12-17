17477fbec1f7a5bdc0234ab7054c8eb0b1686959
V-gy <vgy@hotmail.fr>
2019-12-17 14:38:09 +0100

    luke film

--- javascript-async1/request-url.js
+++ javascript-async1/request-url.js
@@ -7,12 +7,12 @@ request(' https://swapi.co/api/people/1/', function (error, response, body) {
     console.log(luke)
     console.log(luke.films)
 
-    request(' https://swapi.co/api/people/1/', function (error, response, body) {
+    request(luke.films[0], (error, response, body) => {
         console.log('error2:', error); // Print the error if one occurred
         console.log('statusCode2:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body2:', body); // Print the HTML for the Google homepage.
-        const luke = JSON.parse(body)
-        console.log("Luke First Film: " + luke.films[0])
+        const movie = JSON.parse(body)
+        console.log("Luke First Film: " + movie.title)
     })
 }
 );
